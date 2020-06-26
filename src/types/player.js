fd19693f00019a955f41eb32172ace1f1b43a83e
Tobias Berger <tobi.berger13@gmail.com>
2020-06-26 08:40:37 +0200

    Remove temp. JS files; Add sass package

--- src/types/player.js
+++
@@ -1,6 +0,0 @@
-export class Player {
-  constructor () {
-    this.current_tile = 0
-    this.name = 'Spiff'
-  }
-}
--- src/types/resource.js
+++
@@ -1,9 +0,0 @@
-export var Resource;
-(function (Resource) {
-  Resource.Chutzpah = new class Chutzpah {
-  }()
-  Resource.Oxygen = new class Oxygen {
-  }()
-  Resource.Power = new class Power {
-  }()
-})(Resource || (Resource = {}))
