1665f98e15eed0a4e599e215384194bf9b87dbb1
Tobias Berger <tobi.berger13@gmail.com>
2020-07-08 13:06:12 +0200

    Better styling

--- types/time.ts
+++ types/time.ts
@@ -18,7 +18,10 @@ export class Time {
   }
 
   toString () {
-    const s = this.seconds
-    return `${Math.floor(s / (60 * 60 * 24))}d${Math.floor(s / (60 * 60))}h${Math.floor(s / 60)}m${s % 60}s`
+    const days = String(Math.floor(this.seconds / (60 * 60 * 24))).padStart(1, '0')
+    const hours = String(Math.floor(this.seconds / (60 * 60))).padStart(2, '0')
+    const minutes = String(Math.floor(this.seconds / 60)).padStart(2, '0')
+    const seconds = String(this.seconds % 60).padStart(2, '0')
+    return `${days}d${hours}h${minutes}m${seconds}s`
   }
 }
