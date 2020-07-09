4a9772257d610ae4ffbe44d313e939efd3fdf0e2
Tobias Berger <tobi.berger13@gmail.com>
2020-07-09 10:39:26 +0200

    Retry after refactoring Flags and Transformers

--- types/transformers.ts
+++ types/transformers.ts
@@ -89,10 +89,7 @@ export class Transformer {
     ]
 
     apply (model: Model) {
-      if (this.timePoint === undefined) {
-        this.timePoint = model.time.seconds
-      }
-      model.time.seconds = this.timePoint
+      model.time.seconds--
     }
   }
 }
