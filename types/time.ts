e0bb10bc5795323501ff5c73c768e2ac71a5653f
Tobias Berger <tobi.berger13@gmail.com>
2020-07-09 14:41:34 +0200

    Hide useless time parts

--- types/time.ts
+++ types/time.ts
@@ -14,10 +14,15 @@ export class Time {
   }
 
   toString () {
-    const days = String(Math.floor(this.seconds / (60 * 60 * 24))).padStart(1, '0')
-    const hours = String(Math.floor(this.seconds / (60 * 60))).padStart(2, '0')
-    const minutes = String(Math.floor(this.seconds / 60)).padStart(2, '0')
-    const seconds = String(this.seconds % 60).padStart(2, '0')
-    return `${days}d ${hours}h ${minutes}m ${seconds}s`
+    const days = Math.floor(this.seconds / (60 * 60 * 24))
+    const hours = Math.floor(this.seconds / (60 * 60))
+    const minutes = Math.floor(this.seconds / 60)
+    const seconds = this.seconds % 60
+
+    const daysString = days > 0 ? String(days).padStart(1, '0') + 'd ' : ''
+    const hoursString = hours > 0 ? String(hours).padStart(2, '0') + 'h ' : ''
+    const minutesString = minutes > 0 ? String(minutes).padStart(2, '0') + 'm ' : ''
+    const secondsString = String(seconds).padStart(2, '0') + 's'
+    return `${daysString}${hoursString}${minutesString}${secondsString}`
   }
 }
