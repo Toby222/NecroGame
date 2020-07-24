320f4ffa4fb46f75de11d6ea6409ea85f6f0394e
Tobias Berger <tobi.berger13@gmail.com>
2020-07-24 10:53:05 +0200

    dum dum stuffs!
    >:0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,17 @@
-import { Model } from '../components/model'
-
-import Head from 'next/head'
-
 import * as React from 'react'
 
 export default class extends React.Component {
   render() {
     return (
       <>
-        <Head>
+        <head>
           <meta charSet='utf-8' />
           <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
           <meta name='viewport' content='width=device-width, initial-scale=1.0' />
           <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Fira+Code' />
           <link rel='stylesheet' type='text/css' href='impact.css' />
           <title>U r stimky</title>
-        </Head>
+        </head>
         <p>
           Rember 2 taek ur meds uwu<br/>stinky
         </p>
