7d1f8ca3f4f6070957a05c5e6a3424287691a9c7
Tobias Berger <tobi.berger13@gmail.com>
2020-07-28 11:38:27 +0200

    more flavor uwu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 
+const WORDS = ['stinky', 'stinmkly', 'shtomky', 'UwU', 'OwO', 'ÙwÚ', 'ÒwÓ', 'Nya', 'plz', 'pls', 'OmO', 'UmU', 'ÓmÒ', 'ÚmÙ']
+
 export default class extends React.Component {
   render () {
     return (
@@ -10,10 +12,11 @@ export default class extends React.Component {
           <meta name='viewport' content='width=device-width, initial-scale=1.0' />
           <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Fira+Code' />
           <link rel='stylesheet' type='text/css' href='impact.css' />
-          <title>U r stimky</title>
+          <title>{Math.random() > 0.5 ? 'You' : 'U'} {Math.random() > 0.5 ? 'are' : 'r'} {WORDS[Math.floor(Math.random() * 3)]}</title>
         </head>
         <p>
-          Rember 2 taek ur meds uwu<br />stinky
+          Rem{Math.random() > 0.5 ? 'em' : ''}ber {Math.random() > 0.5 ? 'to' : '2'} ta{Math.random() > 0.5 ? 'ke' : 'ek'} {Math.random() > 0.5 ? 'yo' : ''}ur meds<br />
+          {WORDS[Math.floor(Math.random() * WORDS.length)]}
         </p>
       </>
     )
