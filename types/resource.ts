c945268319fca132b00e3e9ae32f5f130fe01f22
Tobias Berger <tobi.berger13@gmail.com>
2020-08-21 10:23:13 +0200

    Beginnings of documentation

--- types/resource.ts
+++ types/resource.ts
@@ -1,8 +1,13 @@
 export abstract class Resource {
+  /** Optional minimum value */
   abstract min?: number;
+  /** Optional maximum value */
   abstract max?: number;
+  /** Initial amount */
   abstract amount: number;
+  /** Initial increase (negative values for initial loss) */
   abstract delta: number;
+  /** Name of the resource */
   abstract name: string;
 }
 
