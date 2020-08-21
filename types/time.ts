23e2f22dac7ca7337ca8936da9c9238b0e4f8084
Tobias Berger <tobi.berger13@gmail.com>
2020-08-21 16:04:29 +0200

    Always write full time

--- types/time.ts
+++ types/time.ts
@@ -19,10 +19,9 @@ export class Time {
     const minutes = Math.floor(this.seconds / 60) % 60;
     const seconds = this.seconds % 60;
 
-    const daysString = days > 0 ? String(days).padStart(1, "0") + "d " : "";
-    const hoursString = hours > 0 ? String(hours).padStart(2, "0") + "h " : "";
-    const minutesString =
-      minutes > 0 ? String(minutes).padStart(2, "0") + "m " : "";
+    const daysString = String(days).padStart(1, "0") + "d ";
+    const hoursString = String(hours).padStart(2, "0") + "h ";
+    const minutesString = String(minutes).padStart(2, "0") + "m ";
     const secondsString = String(seconds).padStart(2, "0") + "s";
     return `${daysString}${hoursString}${minutesString}${secondsString}`;
   }
