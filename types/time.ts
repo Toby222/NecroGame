12bbab1d63b07f1fa9e5c37943c462551c3f89b7
Tobias Berger <tobi.berger13@gmail.com>
2020-08-22 10:21:32 +0200

    Improved time class again

--- types/time.ts
+++ types/time.ts
@@ -1,5 +1,14 @@
 export class Time {
   seconds: number;
+  get minutes() {
+    return Math.trunc(this.seconds / 60);
+  }
+  get hours() {
+    return Math.trunc(this.minutes / 60);
+  }
+  get days() {
+    return Math.trunc(this.hours / 24);
+  }
 
   constructor(val: number | Time = 0) {
     if (val instanceof Time) {
@@ -14,15 +23,18 @@ export class Time {
   }
 
   toString() {
-    const days = Math.trunc(this.seconds / (60 * 60 * 24));
-    const hours = Math.trunc(this.seconds / (60 * 60)) % 24;
-    const minutes = Math.trunc(this.seconds / 60) % 60;
-    const seconds = this.seconds % 60;
+    const days = Math.abs(this.days);
+    const hours = Math.abs(this.hours % 24);
+    const minutes = Math.abs(this.minutes % 60);
+    const seconds = Math.abs(this.seconds % 60);
 
     const daysString = String(days).padStart(1, "0") + "d ";
     const hoursString = String(hours).padStart(2, "0") + "h ";
     const minutesString = String(minutes).padStart(2, "0") + "m ";
     const secondsString = String(seconds).padStart(2, "0") + "s";
-    return `${daysString}${hoursString}${minutesString}${secondsString}`;
+    return (
+      (this.seconds < 0 ? "-" : "") +
+      `${daysString}${hoursString}${minutesString}${secondsString}`
+    );
   }
 }
