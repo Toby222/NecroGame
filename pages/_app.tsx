0aa144a5d7eba225ceb9108c6dc570377a4a5ff5
Tobias Berger <tobi.berger13@gmail.com>
2020-09-07 13:21:18 +0200

    Fix .gitignore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-export { default } from "next/app";
-
 import { config, library } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS
 import { faCog, faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 library.add(faPause, faPlay, faCog);
+
+import App from "next/app";
+export default App;
